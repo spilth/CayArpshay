d2d8db796bd458776ebbd2ff6c844bfc7dbfe3d8
Brian Kelly <polymonic@gmail.com>
2022-02-10 21:31:07 -0600

    CLI accepts a word to translate

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,3 +1,7 @@
 ﻿using CayArpshay;
 
-Console.WriteLine(Translator.Translate("apple"));
+if (args.Length == 1 && args[0].Length > 0) {
+    Console.WriteLine(Translator.Translate(args[0]));
+} else {
+    Console.WriteLine("Please pass a word to translate.");
+}
