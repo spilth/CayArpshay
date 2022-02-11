a69bdd09c5cee44d381e23b52fe47ea1cf6c6d72
Brian Kelly <polymonic@gmail.com>
2022-02-10 21:27:37 -0600

    Handle words starting with lowercase consonants

--- ClassLib/Translator.cs
+++ ClassLib/Translator.cs
@@ -1,8 +1,27 @@
 ﻿namespace CayArpshay;
 
 public static class Translator {
+    private static readonly List<string> Vowels = new() {"a", "e", "i", "o", "u"};
+    
     public static string Translate(string english) {
-        return $"{english}way";
+        string pigLatin;
+
+        if (Vowels.Contains(english[0].ToString())) {
+            pigLatin = $"{english}way";
+        } else {
+            var firstVowelIndex = -1;
+        
+            for (var i = 0; i < english.Length; i++) {
+                if (!Vowels.Contains(english[i].ToString())) continue;
+                
+                firstVowelIndex = i;
+                break;
+            }
+
+            pigLatin = english[firstVowelIndex..] + english[..firstVowelIndex] + "ay";
+        }
+
+        return pigLatin;
     }
 }
 
--- Tests/TranslatorTests.cs
+++ Tests/TranslatorTests.cs
@@ -5,7 +5,15 @@ namespace ClassLibTests;
 
 public class TranslatorTests {
     [Test]
-    public void WordsStartWithLowercaseVowels() {
+    public void WordsStartingWithLowercaseVowels() {
         Assert.That(Translator.Translate("apple"), Is.EqualTo("appleway"));
+        Assert.That(Translator.Translate("orange"), Is.EqualTo("orangeway"));
+    }
+    
+    [Test]
+    public void WordsStartingWithLowercaseConsonants() {
+        Assert.That(Translator.Translate("computer"), Is.EqualTo("omputercay"));
+        Assert.That(Translator.Translate("programming"), Is.EqualTo("ogrammingpray"));
+        Assert.That(Translator.Translate("string"), Is.EqualTo("ingstray"));
     }
 }
